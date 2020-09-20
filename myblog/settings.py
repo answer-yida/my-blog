7067bb61cc17c72477f6af95c3fd5cf70de6c396
answer-yida <score116@163.com>
2020-09-20 17:38:36 +0800

    第二次提交

--- myblog/settings.py
+++ myblog/settings.py
@@ -130,13 +130,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Shanghai'
+
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
+TIME_ZONE = 'Asia/Shanghai'
 
 
 #设置站点
